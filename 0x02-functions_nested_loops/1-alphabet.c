531d9af5403f4ed204abf941e7c0758d4203c3dc
trustdrew <anslemoyiogu@gmail.com>
2022-09-21 04:46:50 -0700

    testing first test

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet -> prints the lowercase alphabets
+ */
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
